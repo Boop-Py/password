1829611bab3c9ba9e96144ee3ac5df70e413bf32
Boop-Py <boopthecode@outlook.com>
2020-02-10 22:55:40 +0000

    Adding password generator file for the first time

---
+++ passwords_generator.py
@@ -0,0 +1,29 @@
+import secrets
+import string
+
+
+# TODO add a way of choosing how many letters/how many digits. Also add a way of asking if user wants another pw made.
+
+
+def make_password(char_no):
+
+    while True:
+ 
+        try: 
+            pw_length = int(char_no)
+            print("Here you go:")
+
+            #Secrets.choice for high entropy character selection.
+            char_list = string.ascii_letters + string.digits
+            password = ''.join(secrets.choice(char_list) for i in range(pw_length)) 
+            print(password) 
+
+        #Re-ask for an integer if invalid input
+        except ValueError:
+            print("Please enter a valid number into funtion: ")
+            continue
+    
+        else:
+            break
+
+make_password(12)
